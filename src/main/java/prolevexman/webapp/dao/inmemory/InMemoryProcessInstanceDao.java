5fa9a2c047564eb57770966962c2c61965b24584
prolevexman <prolevexman@gmail.com>
2026-01-26 20:35:37 +0300

    добавить реализацию dao для хранения в памяти

---
+++ src/main/java/prolevexman/webapp/dao/inmemory/InMemoryProcessInstanceDao.java
@@ -0,0 +1,42 @@
+package prolevexman.webapp.dao.inmemory;
+
+import org.springframework.stereotype.Repository;
+import prolevexman.webapp.dao.ProcessInstanceDao;
+import prolevexman.webapp.model.entity.ProcessInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class InMemoryProcessInstanceDao implements ProcessInstanceDao {
+
+    private final Map<UUID, ProcessInstance> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public void save(ProcessInstance processInstance) {
+        storage.put(processInstance.getId(), processInstance);
+    }
+
+    @Override
+    public void update(ProcessInstance processInstance) {
+        storage.put(processInstance.getId(), processInstance);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        storage.remove(id);
+    }
+
+    @Override
+    public List<ProcessInstance> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+    @Override
+    public ProcessInstance findById(UUID id) {
+        return storage.get(id);
+    }
+}
