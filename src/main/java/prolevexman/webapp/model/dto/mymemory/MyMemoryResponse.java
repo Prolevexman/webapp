ca4c7004ba00e1f6b39a846a604c49b13d4a0697
prolevexman <prolevexman@gmail.com>
2026-01-28 12:47:08 +0300

    добавить dto MyMemory сервиса перевода

---
+++ src/main/java/prolevexman/webapp/model/dto/mymemory/MyMemoryResponse.java
@@ -0,0 +1,10 @@
+package prolevexman.webapp.model.dto.mymemory;
+
+public class MyMemoryResponse {
+
+    private ResponseData responseData;
+
+    public MyMemoryResponse(ResponseData responseData) {
+        this.responseData = responseData;
+    }
+}
---
+++ src/main/java/prolevexman/webapp/model/dto/mymemory/ResponseData.java
@@ -0,0 +1,10 @@
+package prolevexman.webapp.model.dto.mymemory;
+
+public class ResponseData {
+
+    private String translatedText;
+
+    public ResponseData(String translatedText) {
+        this.translatedText = translatedText;
+    }
+}
